1c57b31954e6d91cf36f969c85f94249bbdaf166
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2024-01-29 12:00:53 +0545

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		"app/**/*.html",
+		"app/**/*.{jsx,tsx,js,ts}}",
+		"components/**/*.{jsx,tsx,js,ts}}",
+		"constants/**/*.{jsx,tsx,js,ts}}",
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
